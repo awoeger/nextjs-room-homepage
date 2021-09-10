12ac08f0f64c9628e7bc40182b6470fed0378f95
Andrea Wöger <andrea.woeger@gmx.net>
2021-09-10 11:08:31 +0200

    Add shared styles

---
+++ styles/styles.js
@@ -0,0 +1,13 @@
+// Layout
+// The designs were created to the following widths:
+
+// - Mobile: 375px
+// - Desktop: 1440px
+
+// Font
+// - Family: [Spartan](https://fonts.google.com/specimen/Spartan)
+// - Weights: 500, 600, 700
+
+// Primary Colors:
+export const lightGray = '#a1a1a1';
+export const darkGray = '#454545';
