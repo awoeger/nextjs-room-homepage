36c407b9cbe628c2c8cb11ea920f4d7b495fb305
Andrea Wöger <andrea.woeger@gmx.net>
2021-09-13 11:19:33 +0200

    Create Array for Image Slider

---
+++ components/SliderData.js
@@ -0,0 +1,14 @@
+export const sliderImages = [
+  {
+    id: 1,
+    src: '/../public/images/mobile/mobile-image-hero-1.jpg',
+  },
+  {
+    id: 2,
+    src: '/../public/images/mobile/mobile-image-hero-2.jpg',
+  },
+  {
+    id: 3,
+    src: '/../public/images/mobile/mobile-image-hero-3.jpg',
+  },
+];
