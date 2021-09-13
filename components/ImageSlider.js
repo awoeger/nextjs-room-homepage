7d78da80b8ec61c0cd0a994e226a89aa18d0d2ea
Andrea Wöger <andrea.woeger@gmx.net>
2021-09-13 16:26:41 +0200

    Add styles for slide show

--- components/ImageSlider.js
+++ components/ImageSlider.js
@@ -1,22 +1,77 @@
+import { css } from '@emotion/react';
 import Image from 'next/image';
+import { useState } from 'react';
+import * as RiIcons from 'react-icons/ri';
 import { sliderImages } from './SliderData';
 
+const slider = css`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  button {
+    background: black;
+    border: none;
+    cursor: pointer;
+  }
+
+  .leftArrow {
+    position: absolute;
+    bottom: 5px;
+    right: 40px;
+  }
+
+  .rightArrow {
+    position: absolute;
+    bottom: 5px;
+    right: 0px;
+  }
+`;
+
 function ImageSlider() {
+  const [currentSlide, setCurrentSlide] = useState(1);
+  const length = sliderImages.length;
+
+  const previousSlide = () => {
+    setCurrentSlide(currentSlide === length ? 1 : currentSlide - 1);
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === length ? 1 : currentSlide + 1);
+  };
+
+  console.log(currentSlide);
+  console.log(length);
+
   return (
-    <>
+    <section css={slider}>
       {sliderImages.map((slide) => {
         return (
-          <div key={slide.id}>
-            <Image
-              width="375px"
-              height="360px"
-              src={slide.src}
-              alt="Room furniture"
-            />
+          <div
+            className={slide.id === currentSlide ? 'slideActive' : 'slide'}
+            key={slide.id}
+          >
+            {slide.id === currentSlide && (
+              <Image
+                width="375px"
+                height="360px"
+                src={slide.src}
+                alt="Room furniture"
+                className="image"
+              />
+            )}
           </div>
         );
       })}
-    </>
+      <button className="leftArrow" onClick={previousSlide}>
+        <RiIcons.RiArrowLeftSLine color="white" size={40} />
+      </button>
+      <button className="rightArrow" onClick={nextSlide}>
+        <RiIcons.RiArrowRightSLine color="white" size={40} />
+      </button>
+    </section>
   );
 }
 
