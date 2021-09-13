c70acc1320da4a1137ec02594ba5472fa14ccf80
Andrea Wöger <andrea.woeger@gmx.net>
2021-09-13 19:27:14 +0200

    Add global style for headings

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { css, Global } from '@emotion/react';
+import { darkGray, lightGray } from '../styles/styles';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -13,6 +14,20 @@ function MyApp({ Component, pageProps }) {
           body {
             margin: 0;
             padding: 0;
+          }
+          h1,
+          h2,
+          h3,
+          h4 {
+            font-family: 'Spartan', sans-serif;
+            color: ${darkGray};
+            padding: 0 25px;
+          }
+
+          p {
+            font-family: 'Spartan', sans-serif;
+            color: ${lightGray};
+            padding: 0 25px;
           }
         `}
       />
