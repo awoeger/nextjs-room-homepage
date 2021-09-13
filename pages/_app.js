59fb255aafac6de06b4ce8944c40260a86176de2
Andrea Wöger <andrea.woeger@gmx.net>
2021-09-13 16:26:20 +0200

    Add Global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
+import { css, Global } from '@emotion/react';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global
+        styles={css`
+          *,
+          *::after,
+          *::before {
+            box-sizing: border-box;
+          }
+          body {
+            margin: 0;
+            padding: 0;
+          }
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
